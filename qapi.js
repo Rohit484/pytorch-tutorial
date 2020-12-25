c35f5e69ed25235a82c6e967a7d506e045af9534
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2020-12-25 23:51:28 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fe62cba6d47ab001e034a9b",
+				"5fe62cba6d47ab001e034a9d",
+				"5fe62d0694657a0021e7d584"
+			]
+		},
+		"5fe62cba6d47ab001e034a9b": {
+			"id": "5fe62cba6d47ab001e034a9b",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fe62cba6d47ab001e034a9d": {
+			"id": "5fe62cba6d47ab001e034a9d",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5fe62d0694657a0021e7d584": {
+			"id": "5fe62d0694657a0021e7d584",
+			"pageUrl": "1",
+			"name": "1"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
